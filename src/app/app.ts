9c2cf172c4336cfa410bbc331ada6ac1861fa17a
Arturo Bernal <arturober@gmail.com>
2026-03-20 14:04:20 +0100

    Primeros pasos de configuración del menú lateral con botón

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatDrawer, MatDrawerContainer, MatDrawerContent } from '@angular/material/sidenav';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatDrawer, MatDrawerContainer, MatDrawerContent, MatToolbar, MatIconButton, MatIcon],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
-  protected readonly title = signal('novagenda-front');
+  sideMenuOpen = signal(false);
 }
---
+++ src/app/shared/components/top-bar/top-bar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'top-bar',
+  imports: [],
+  templateUrl: './top-bar.html',
+  styleUrl: './top-bar.css',
+})
+export class TopBar {}
